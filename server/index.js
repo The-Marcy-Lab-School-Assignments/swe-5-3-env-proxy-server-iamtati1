a48af6ac272bf253cd28983e3e6b402fb4324d19
iamtati1 <tatianabarmer@gmail.com>
2026-03-11 12:01:25 -0400

    Setup dotenv and environment variable for Giphy API

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 //////////////////////////
 // Imports
 //////////////////////////
-
+const fetch = require("node-fetch").default;
 const path = require('path');
 const express = require('express');
+require('dotenv').config();
 
+const apiKey = process.env.GIPHY_API_KEY;
+console.log(apiKey);
 //////////////////////////
 // Constants
 //////////////////////////
@@ -13,6 +16,17 @@ const port = 8080;
 const pathToFrontend = path.join(__dirname, '../frontend');
 const app = express();
 
+function getGifs(req, res, next) {
+    fetch("https://api.giphy.com/v1/gifs/trending?limit=3&rating=g&api_key=9q65JJFPUpJYhCd3WSSzeju5aDxacYol")
+        .then((response) => response.json())
+        .then(data => res.json(data))
+        .catch((error) => {
+            console.log("FETCH ERROR:", error);
+            res.status(503).json({ error: "Error fetching gifs" });
+        });
+}
+// Function gets list of GIFS
+app.get('/api/gifs', getGifs); // This registers the API endpoint
 //////////////////////////
 // Middleware/Controllers
 //////////////////////////
